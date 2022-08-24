cbf53c39968f041e64c842fdda2ee2d8260f5722
alg0r1thmm <jeongsw1217@gmail.com>
2022-08-24 13:31:32 +0900

    cpp file

---
+++ widget.cpp
@@ -0,0 +1,104 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    /*if (diff>=500){
+        ui->pbCoffee->setEnabled(false);
+        ui->pbMilk->setEnabled(false);
+        ui->pbMango->setEnabled(false);
+    }*/
+    ui->pbCoffee->setEnabled(false);
+    ui->pbMango->setEnabled(false);
+    ui->pbMilk->setEnabled(false);
+    //ui->pbCoffee(500)->isEnabled(true);
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeMoney(int diff)
+{
+    money += diff;
+    ui->lcdNumber->display(money);
+    if(money >= 500){
+        ui->pbCoffee->setEnabled(true);
+        ui->pbMango->setEnabled(true);
+        ui->pbMilk->setEnabled(true);
+    }
+    else if(money >= 300){
+        ui->pbMango->setEnabled(true);
+        ui->pbCoffee->setEnabled(true);
+    }
+    else if(money >= 100){
+        ui->pbCoffee->setEnabled(true);
+    }
+    if(money < 100){
+        ui->pbCoffee->setEnabled(false);
+        ui->pbMango->setEnabled(false);
+        ui->pbMilk->setEnabled(false);
+    }
+    else if(money < 300){
+        ui->pbMango->setEnabled(false);
+        ui->pbMilk->setEnabled(false);
+    }
+    else if(money < 500){
+        ui->pbMilk->setEnabled(false);
+    }
+
+}
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+void Widget::on_pbMango_clicked()
+{
+    changeMoney(-300);
+}
+
+void Widget::on_pbMilk_clicked()
+{
+    changeMoney(-500);
+}
+
+void Widget::on_pbReset_clicked()
+{
+    QMessageBox msg1;
+    QMessageBox msg2;
+    msg1.information(nullptr,"money","!!reset!!");
+    msg2.information(nullptr,"money","!!reset!!");
+    money = 0;
+    ui->lcdNumber->display(money);
+    ui->pbCoffee->setEnabled(false);
+    ui->pbMango->setEnabled(false);
+    ui->pbMilk->setEnabled(false);
+}
